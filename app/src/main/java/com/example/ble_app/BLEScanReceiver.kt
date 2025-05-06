41d07b8d2610dd0625246cec9393fbe900f4e3d7
cht-au <145236495+cht-au@users.noreply.github.com>
2025-05-06 13:27:46 -0400

    added timestamp

--- app/src/main/java/com/example/ble_app/BLEScanReceiver.kt
+++ app/src/main/java/com/example/ble_app/BLEScanReceiver.kt
@@ -3,12 +3,12 @@ package com.example.ble_app
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.getIntent
 import android.util.Log
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.BluetoothLeScanner
 import android.os.Build
 import androidx.annotation.RequiresApi
+import java.sql.Timestamp
 
 
 class BLEScanReceiver : BroadcastReceiver() {
@@ -24,11 +24,15 @@ class BLEScanReceiver : BroadcastReceiver() {
                 val device = result.device
                 val address = device.address
                 val rssi = result.rssi
-                Log.d("BLEScanReceiver", "Device found: $address, RSSI: $rssi")
+
+                val currentTimeMillis = System.currentTimeMillis()
+                val timeStamp = Timestamp(currentTimeMillis)
+
+                Log.d("BLEScanReceiver", "Device found: $address, RSSI: $rssi, time: $timeStamp")
                 val notificationHelper = NotificationHandler(context)
                 notificationHelper.buildNotification(
                     title = "BLE Device Detected",
-                    message = "Address: $address, RSSI: $rssi"
+                    message = "Address: $address, RSSI: $rssi, Time: $timeStamp"
                 )
             }
         }
